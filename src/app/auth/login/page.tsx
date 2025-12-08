bc18d2f24c253be9583d18f32d6596afbb6154d2
faatihahr <fahraaraa@gmail.com>
2025-12-08 09:26:15 +0700

    fix: reorder import statements in login and signup pages

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,8 +1,8 @@
 'use client';
 
-import { useState, useEffect } from 'react';
-
 export const dynamic = 'force-dynamic';
+
+import { useState, useEffect } from 'react';
 import { useRouter, useSearchParams } from 'next/navigation';
 import { login } from './actions';
 import { FiBook, FiMoon, FiSun } from 'react-icons/fi';
